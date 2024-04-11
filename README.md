a07885f9c2c5034ea3140eb8359447098a9721aa
Rohan Singh <rohansinghrajput.dev@gmail.com>
2024-04-11 14:21:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# API Docs
+
+## 1. '/today': returns a list of today's updated comics (latest update)
+
+```json
+[
+    {
+        "id": "manga-eb981436",
+        "thumb": "https://avt.mkklcdnv6temp.com/14/r/19-1583497652.jpg",
+        "title": "In The Sunroom"
+    },
+    {
+        "id": "manga-dv981004",
+        "thumb": "https://avt.mkklcdnv6temp.com/49/f/17-1583497041.jpg",
+        "title": "Boku no Kokoro no Yabai yatsu"
+    },
+    .
+    .
+    .
+]
+```
+
+
+## 2. '/hot': returns a list of hottest/trending comics
+
+```json
+[
+  {
+    "id": "manga-ax951880",
+    "thumb": "https://avt.mkklcdnv6temp.com/19/v/1-1583464475.jpg",
+    "title": "Tales of Demons and Gods"
+  },
+  {
+    "id": "manga-dr980474",
+    "thumb": "https://avt.mkklcdnv6temp.com/30/a/17-1583496340.jpg",
+    "title": "Solo Leveling"
+  },
+  .
+  .
+  .
+]
+```
+
+## 3. '/id': Returns a single comic's information by id
+```json
+{
+    "about": "Nie Li, one of the strongest Demon Spiritist in his past life standing at the pinnacle of the martial world , however he lost his life during the battle with Sage Emperor and the six deity ranked beast, his soul was then reborn back in time back to when he is still 13. Although he’s the weakest in his class with the lowest talent at only Red soul realm, with the aid of the vast knowledge which he accumulated in his previous life, he trained faster then anyone. Trying to protect the city which in the coming future was being assaulted by beast and ended up being destroyed as well as protecting his lover, friends and family who died by the beast assault. and to destroy the Sacred family whom abandon their duty and betrayed the city in his past life.",
+    "genre": [
+        "Action",
+        "Adventure",
+        "Comedy",
+        "Drama",
+        "Fantasy",
+        "Harem",
+        "Romance",
+        "Shounen",
+        "Manhua"
+    ],
+    "thumb": "https://avt.mkklcdnv6temp.com/19/v/1-1583464475.jpg",
+    "title": "Tales Of Demons And Gods"
+}
+```
+
+## 4. '/id/episodes': returns the latest episodes for a comic by id
+```json
+[
+  {
+    "date": "Apr 10,24",
+    "id": "manga-ax951880",
+    "link": "https://chapmanganato.to/manga-ax951880/chapter-470.1",
+    "thumb": "https://avt.mkklcdnv6temp.com/19/v/1-1583464475.jpg",
+    "title": "Chapter 470.1",
+    "view": "40.2K"
+  },
+  {
+    "date": "Apr 06,24",
+    "id": "manga-ax951880",
+    "link": "https://chapmanganato.to/manga-ax951880/chapter-469.5",
+    "thumb": "https://avt.mkklcdnv6temp.com/19/v/1-1583464475.jpg",
+    "title": "Chapter 469.5",
+    "view": "60.3K"
+  },
+  .
+  .
+  .
+]
+```
---
+++ api.py
@@ -0,0 +1,139 @@
+from flask import Flask, redirect, jsonify, render_template
+from flask_bootstrap import Bootstrap4
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+bootstrap = Bootstrap4(app)
+
+HEADERS = {  
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+    "Accept-Language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,ar;q=0.6,hi;q=0.5"
+}
+
+# Fetch the webpage content
+def get_soup(url):
+    res = requests.get(url, headers=HEADERS)
+    comic_page = res.text
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(comic_page, "lxml")
+    return soup
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/hot')
+def get_hottest():
+    soup = get_soup('https://manganato.com/genre-all?type=topview')
+    hottest_comics = soup.find_all(class_='content-genres-item')   # list of html elements
+    all_comics = []  # list of dicts
+    for item in hottest_comics:
+        all_comics.append({
+            'title': item.find(name='a').get('title'),
+            'id': item.find(name='a').get('href').split('/')[-1],  # slug: manga-ax951880
+            # 'link': item.find(name='a').get('href'),
+            'thumb': item.find(name='img').get('src'),
+            # 'about': item.find(class_='genres-item-description').text.strip(),
+            # 'author': item.find(class_='genres-item-author').text
+        })
+    return jsonify(all_comics)
+
+@app.route('/today')
+def get_latest():
+    soup = get_soup('https://manganato.com/')
+    latest_comics = soup.find_all(class_='content-homepage-item')   # list of html elements
+    all_comics = []  # list of dicts
+    for item in latest_comics:
+        all_comics.append({
+            'title': item.find(name='a').get('title'),
+            'thumb': item.find(name='img', class_='img-loading').get('src'),
+            'id': item.find(name='a').get('href').split('/')[-1],  # slug: manga-ax951880
+        })
+    return jsonify(all_comics)
+
+# '/id': Returns a single comic's information by id
+# Eg: /manga-ax951880
+@app.route('/<string:id>')
+def get_comic_info(id):
+    soup = get_soup(f"https://chapmanganato.to/{id}")
+    comic = soup.find(name='div', class_='panel-story-info')   # list of html elements
+    if comic is None:
+        return jsonify({'error': 'Comic not found'})
+    return jsonify({
+        'title': comic.find('h1').text,
+        'about': comic.select_one('.panel-story-info-description').contents[-1].strip(),
+        'genre': [genre.text for genre in comic.select(selector='.table-value .a-h') if genre.get('href').startswith('https://manganato.com/genre')],
+        'thumb': comic.find(class_='img-loading').get('src'),
+    })
+
+# '/id/episodes': returns the latest episodes for a comic by id
+@app.route('/<string:id>/episodes')
+def get_comic_chapters(id):
+    soup = get_soup(f'https://chapmanganato.to/{id}')
+    thumb = soup.select_one('.info-image .img-loading').get('src')
+    chapter_tags = soup.find(class_='row-content-chapter').find_all(name='li', class_='a-h')
+    if chapter_tags is None:
+        return jsonify({'error': 'Comic not found'})
+    comic_chapters = []
+    for tag in chapter_tags:
+        comic_chapters.append({
+            'thumb': thumb,
+            'id': id,
+            'link': tag.find(name='a', class_='chapter-name').get('href'),
+            'title': tag.find(name='a', class_='chapter-name').text,
+            'view': tag.find(name='span', class_='chapter-view').text,
+            'date': tag.find(name='span', class_='chapter-time').text,
+        })
+    return jsonify(comic_chapters)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+# for itm in all_comics:
+#     print(itm.title)
+#     print(itm.id)
+#     print(itm.link)
+#     print(itm.thumb)
+#     print(itm.about)
+#     print(itm.author)
+
+
+# chapter link -> https://chapmanganato.to/id
+# ------------------------------------------------------------------
+# 1. '/today'  -> returns today's comics (latest update)
+# [
+#  {
+#   'title': '...',
+#   'thumb': 'https://...',
+#   'id': '790713'
+#  }
+# ]
+
+# -------------------------
+# 2. '/id'  -> Returns a single comic's information by id
+# {
+#     'title': '...',
+#     'about': '...',
+#     'genre': '...',
+#     'age': '...',
+#     'thumb': '...'
+# }
+
+# ---------------------------
+# 3. '/id/episodes'  -> returns the latest episodes for a comic by id
+# [
+#   {
+#     'thumb': '...'
+#     'id': '790713'
+#     'title': '...',
+#     'rating': '9.98',
+#     'date': '...',
+#   }
+#   {...},
+#   {...},
+# ]
